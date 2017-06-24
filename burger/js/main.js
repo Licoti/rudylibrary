45c37606ecc9e668c01c8a076938635d49257a77
Rudy Li <Rudy Li>
2017-06-24 18:53:08 +0200

    Ajout du clone sur le menu

--- burger/js/main.js
+++ burger/js/main.js
@@ -1,33 +1,34 @@
 var r_burgerMenuGlobal = {
-    init: function () {
-        this.debug();
-        this.openCloseMenu();
-        this.cloneMenu();
-    },
-    debug: function (){
-        console.log('Hello !')
-    },
-    openCloseMenu: function () {
-        var self = this;
+  init: function () {
+    this.debug();
+    this.cloneMenu();
+    this.openCloseMenu();
+  },
+  debug: function (){
+    console.log('Hello !')
+  },
+  openCloseMenu: function () {
+    var self = this;
 
-        self.config ={
-            _buttonMenu: document.querySelectorAll('.r_navigation__title--burger'),
-            _contentMenu: document.querySelectorAll('.r_navigation')
-        };
+    self.config ={
+      _buttonMenu: document.querySelectorAll('.r_navigation__title--burger'),
+      _contentMenu: document.querySelectorAll('.r_cloned-navigation')
+    };
 
-        [].forEach.call(document.querySelectorAll('.r_navigation__title--burger'), function(el) {
-            el.addEventListener('click', function() {
-                [].map.call(self.config._contentMenu, function(element) {
-                    element.classList.toggle("active");
-                });
-            });
-        });
-    },
-    cloneMenu: function () {
+    [].forEach.call(self.config._buttonMenu, function(el) {
+      el.addEventListener('click', function() {
+          [].map.call(self.config._contentMenu, function(element) {
+              element.classList.toggle("active");
+          });
+      });
+    });
+  },
+  cloneMenu: function () {
+    var el = document.querySelector('.r_main-navigation');
+    var clonedElement = el.cloneNode(true);
 
-    }
+    document.querySelector('.r_cloned-navigation').appendChild(clonedElement);
+  }
 };
 
-document.addEventListener('DOMContentLoaded', function() {
-    r_burgerMenuGlobal.init();
-});
+r_burgerMenuGlobal.init();
