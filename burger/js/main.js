909944e27f597b884b1cf91a88d5dd10a37310da
Rudy Zourane <rudy.zourane@neolynk.fr>
2017-06-29 14:44:44 +0200

    Modification sur le JS

--- burger/js/main.js
+++ burger/js/main.js
@@ -1,9 +1,20 @@
 var r_burgerMenuGlobal = {
   init: function () {
+
     this.config ={
+      CSS:{
+        classes:{
+          opened:'opened',
+          active: 'active'
+        },
+        ids:{
+          container:'maincontainer'
+        }
+      },
       _buttonMenu: '.r_navigation__title--burger',
       _contentMenu: '.r_main-navigation',
       _contentMenuCloned: '.r_cloned-navigation',
+        isOpen: false
     };
 
     this.cloneMenu();
@@ -12,30 +23,45 @@ var r_burgerMenuGlobal = {
     this.debug();
   },
 
-  debug: function (){
+  debug: function () {
     console.log('Hello !');
   },
 
   toggleMenu: function () {
     [].forEach.call(document.querySelectorAll(this.config._buttonMenu), function(el) {
-      el.addEventListener('click', function() {
-        [].map.call(document.querySelectorAll(r_burgerMenuGlobal.config._contentMenuCloned), function(element) {
-            element.classList.toggle("active");
-        });
-      });
+      el.addEventListener('click', clickOpenMenu);
     });
+
+    function clickOpenMenu () {
+        [].map.call(document.querySelectorAll(r_burgerMenuGlobal.config._contentMenuCloned), function(element) {
+            element.classList.toggle(r_burgerMenuGlobal.config.CSS.classes.opened);
+
+            r_burgerMenuGlobal.config.isOpen = true;
+        });
+    }
+
+
   },
 
   outsideMenu: function () {
-    window.addEventListener('click', function(e){
+/*    window.addEventListener('click', function(e){
       if (!document.getElementById('cloned-menu').contains(e.target)){
-        if (document.querySelector(r_burgerMenuGlobal.config._contentMenuCloned).classList.contains('active')) {
-          alert("Clicked in Box");
-          document.querySelector(r_burgerMenuGlobal.config._contentMenuCloned).classList.remove('active');
+        if (document.querySelector(r_burgerMenuGlobal.config._contentMenuCloned).classList.contains(r_burgerMenuGlobal.config.CSS.classes.active)) {
+          alert("Clicked out Box");
+          document.querySelector(r_burgerMenuGlobal.config._contentMenuCloned).classList.remove(r_burgerMenuGlobal.config.CSS.classes.opened);
         }
       }
-    });
+    });*/
+
+      window.addEventListener( 'click', function(ev) {
+          var target = ev.target;
+
+          if( r_burgerMenuGlobal.config.isOpen == true && !document.getElementById('cloned-menu').contains(ev.target)) {
+              alert('hello');
+          }
+      });
   },
+
 
   cloneMenu: function () {
     var el = document.querySelector(this.config._contentMenu),
@@ -46,6 +72,16 @@ var r_burgerMenuGlobal = {
   }
 };
 
-(function() {
-  r_burgerMenuGlobal.init();
-}());
+;(function(window) {
+
+    'use strict';
+    r_burgerMenuGlobal.init();
+
+})(window);
+
+
+
+
+
+
+
